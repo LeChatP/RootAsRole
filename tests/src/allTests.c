70314c55b368cff0ddd80e390883daac4622d4cc
LeChatP <lechatp@outlook.fr>
2019-04-29 11:52:35 +0200

    adding colors in test (visualize fails)

--- tests/src/allTests.c
+++ tests/src/allTests.c
@@ -11,17 +11,17 @@ int registerTest(TestSuite *testSuite, Test *test){
 
 int trigger(TestSuite *suite,int notification){
     void* callstack[128];
-    if(notification)printf("====== running TestSuite %s ======\n",suite->name);
+    if(notification)printf("\033[0;34m====== running TestSuite %s ======\033[0m\n",suite->name);
     jmp_buf testjmp;
     int return_code = 1;
     if(!setjmp(testjmp)){ //try
         for(int i = 0 ; i<suite->size;i++){
-            if(notification)printf("running test %s()\n",suite->tests[i]->name);
+            if(notification)printf("\033[0;34mrunning test %s()\033[0m\n",suite->tests[i]->name);
             if(return_code){
                 return_code = ((suite->tests[i]->impl)());
-                if((!return_code) && notification) printf("Ce test a échoué\n");
+                if((!return_code) && notification) printf("\033[1;31mCe test a échoué\033[0m\n");
             }else{
-                if((!(suite->tests[i]->impl)())&& notification) printf("Ce test a échoué\n");
+                if((!(suite->tests[i]->impl)())&& notification) printf("\033[1;31mCe test a échoué\033[0m\n");
             }
         }
     }else // catch
