ce8854795a9808a9b73ba09b9e3ac663c4c5abc1
anderseee <77062357+anderseee@users.noreply.github.com>
2022-01-28 16:13:30 +0100

    Update verifier.c

--- role-manager/verifier.c
+++ role-manager/verifier.c
@@ -1,88 +1,87 @@
+#include <errno.h>
+#include <grp.h>
+#include <libxml/parser.h>
+#include <libxml/xpath.h>
+#include <pwd.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <pwd.h>
-#include <grp.h>
-#include <errno.h>
 #include <sys/capability.h>
-#include "role_manager.h"
+#include <unistd.h>
+
+#include "xmlNode.h"
+#include "verifier.h"
 
 extern int errno;
 
+/* @return : -1 to failure | 0 to success */
+int root_verifier(void)
+{
+    if (!getuid())
+        return 0;
+    else {
+        fputs("For run this command you must be root user !\n", stderr);
+        return -1;
+    }
+}
+
+
+/* Doit on valider la DTD avant de valider le document par la DTD ?
+ * https://stackoverflow.com/questions/4594049/dtd-validation-with-libxml2
+ * https://www.julp.fr/articles/1-4-validation-d-un-document-xml.html
+ * Si oui, cela implique de mapper la mémoire avec la vrai DTD,
+ * pour ensuite la comparer avec la DTD du fichier, ou bien retirer
+ * la DTD du fichier pour ne laisser que la DTD en mémoire effectuer
+ * les vérifications.
+ *
+ * @return : NULL to error | doc to success
+ */
 xmlDocPtr xml_verifier(void)
 {
-    int error = 0;
     xmlParserCtxtPtr ctxt;
     xmlDocPtr doc;
-    xmlNodePtr cur_node;
 
     ctxt = xmlNewParserCtxt();
-    if (ctxt == NULL) {
+    if (!ctxt) {
         fputs("Failed to allocate parser context\n", stderr);
         return NULL;
     }
 
-    doc = xmlCtxtReadFile(ctxt, XML_FILE, NULL, XML_PARSE_DTDVALID);
-    if (doc == NULL) {
+    doc = xmlCtxtReadFile(ctxt, XML_FILE, NULL, XML_PARSE_DTDVALID|XML_PARSE_NOBLANKS);
+    if (!doc) {
         fprintf(stderr, "Failed to parse %s\n", XML_FILE);
-        error = 1;
+        goto ret_err;
     }
-    else {
-        if (ctxt->valid == 0) {
-            fprintf(stderr, "Failed to validate %s\n", XML_FILE);
-            error = 1;
-        }
+    if (!ctxt->valid) {
+        fprintf(stderr, "Failed to validate %s\n", XML_FILE);
         xmlFreeDoc(doc);
+        goto ret_err;
     }
 
     xmlFreeParserCtxt(ctxt);
-    if (error)
-        return NULL;
 
-    xmlKeepBlanksDefault(0);
-    doc = xmlParseFile("./file.xml");
-    //doc = xmlReadFile(XML_FILE, NULL, 0);
-    cur_node = xmlDocGetRootElement(doc);
-
-    if (xmlStrcmp(cur_node->name, BAD_CAST "capabilityrole") != 0) {
-        fputs("Root element would be <capabilityrole>\n", stderr);
-        error = 1;
-        goto ret_gt;
-    }
-
-    if (cur_node->children == NULL) {
-        fputs("Root element must have <roles> child !\n", stderr);
-        error = 1;
-        goto ret_gt;
-    }
-
-    cur_node = cur_node->children;
-
-    if (xmlStrcmp(cur_node->name, BAD_CAST "roles") != 0) {
-        fprintf(stderr, "Child element would be <roles> but not <%s>\n", (char*)cur_node->name);
-        error = 1;
-    }
-
-ret_gt:
-    if (error) {
-        xmlFreeDoc(doc);
-        return NULL;
-    }
     return(doc);
+
+ret_err:
+    xmlFreeParserCtxt(ctxt);
+    return NULL;
 }
 
-
+/* @role_node is optionnal. NULL for not use
+ * @return : -1 to error | 0 if role doesn't exist | 1 if role exists
+ */
 int role_verifier(xmlDocPtr doc, xmlNodePtr *role_node, char *role)
 {
-    xmlNodePtr root_node = NULL;
-    xmlNodePtr cur_node = NULL;
+    xmlChar *expression = NULL;
+    int ret;
 
     if (!strcmp(role, "")) {
         fputs("Role is empty\n", stderr);
         return -1;
     }
     if (strlen(role) >= MAX_ROLE_LEN) {
-        fprintf(stderr, "Role is too long : %d charchters max\n", MAX_ROLE_LEN);
+        fprintf(stderr, "Role is too long : %d characters max\n", MAX_ROLE_LEN);
         return -1;
     }
     if (strchr(role, '\'') != NULL && strchr(role, '"') != NULL) {
@@ -90,38 +89,36 @@ int role_verifier(xmlDocPtr doc, xmlNodePtr *role_node, char *role)
         return -1;
     }
 
-    root_node = xmlDocGetRootElement(doc);
-    cur_node = root_node;
+    expression = newXPression(role, 0, NULL);
+    if (!expression)
+        return -1;
 
-    do {
-        cur_node = cur_node->children;
-        if (cur_node->type == XML_ELEMENT_NODE && !xmlStrcmp(cur_node->name, BAD_CAST "roles"))
-            root_node = cur_node;
-        if (cur_node->type == XML_ELEMENT_NODE && !xmlStrcmp(cur_node->name, BAD_CAST "role")) {
-            for (;cur_node; cur_node = cur_node->next) {
-                *role_node = cur_node;
-                if (cur_node->type == XML_ELEMENT_NODE && !xmlStrcmp(xmlGetProp(cur_node, BAD_CAST "name"), BAD_CAST role)) {
-                    return 1;
-                }
-            }
-        }
-    } while(cur_node != NULL);
+    ret = researchNode(doc, expression, role_node, NULL);
 
-    *role_node = root_node;
-
-    return 0;
+    free(expression);
+    return ret;
 }
 
 
-int capability_verifier(char *cap_text, args_struct *args)
+/* @capability[43] is optionnal. NULL for not use
+ * @return : -1 to error | 0 success
+ */
+int capability_verifier(char *cap_text, bool capability[43])
 {
     char *token;
     cap_value_t capVal;
 
     token = strtok(cap_text, ",");
+
+    if (token == NULL) {
+        fputs("Capability is empty\n", stderr);
+        return -1;
+    }
+
     do {
         if (!strcmp(token, "*")) {
-            args->capability[42] = true;
+            if (capability)
+                capability[42] = true;
             return 0;
         }
         if (cap_from_name(token, &capVal) == -1) {
@@ -129,85 +126,111 @@ int capability_verifier(char *cap_text, args_struct *args)
             return -1;
         }
 
-        args->capability[capVal] = true;
+        if (capability)
+            capability[capVal] = true;
     } while ( (token = strtok(NULL, ",")) != NULL);
 
     return 0;
 }
 
 
-int user_verifier(char *users, args_struct *args)
+/* @return : -1 to error | User number to success */
+int user_verifier(char *users)
 {
     char *token;
+    int i;
 
     token = strtok(users, ",");
 
-    do {
-        if (args->uc == 10) {
-            fputs("10 users max can be added\n", stderr);
+    for (i = 0; token != NULL; i++) {
+        if (i == MAX_BLOC) {
+            fputs("Limits for user blocs reached\n", stderr);
             return -1;
         }
+
+        if (strchr(token, '\'') != NULL && strchr(token, '"') != NULL) {
+            fputs("You cannot set quote and apostrophe in a parameter due to XML restrictions\n", stderr);
+            return -1;
+        }
+
         if (strlen(token) >= MAX_NAME_LEN) {
             fprintf(stderr, "Username is too long -> %d characters max\n", MAX_NAME_LEN);
             return -1;
         }
 
         errno = 0;
-        if (getpwnam(token) == NULL) {
+        if ( (getpwnam(token)) == NULL) {
             if (errno != 0)
                 perror("getpwnam()");
             else
                 fprintf(stderr, "\"%s\" : Username doesn't exist\n", token);
             return -1;
-            }
+        }
+        token = strtok(NULL, ",");
+    }
 
-            args->uc++;
-    } while ( (token = strtok(NULL, ",")) != NULL);
+    if (!i) {
+        fputs("User is empty\n", stderr);
+        return -1;
+    }
 
-    return 0;
+    return i;
 }
 
 
-int group_verifier(char *groups, args_struct *args)
+/* @return : -1 to error | Group number to success */
+int group_verifier(char *groups)
 {
     char *token;
+    int i;
 
     token = strtok(groups, ",");
 
-    do {
-        if (args->gc == 10) {
-            fputs("10 groups max can be added\n", stderr);
+    for (i = 0; token != NULL; i++) {
+        if (i == MAX_BLOC) {
+            fputs("Limits for user blocs reached\n", stderr);
             return -1;
         }
+
+        if (strchr(token, '\'') != NULL && strchr(token, '"') != NULL) {
+            fputs("You cannot set quote and apostrophe in a parameter due to XML restrictions\n", stderr);
+            return -1;
+        }
+
         if (strlen(token) >= MAX_NAME_LEN) {
             fprintf(stderr, "Group is too long -> %d characters max\n", MAX_NAME_LEN);
             return -1;
         }
 
         errno = 0;
-        if (getgrnam(token) == NULL) {
+        if ( (getgrnam(token)) == NULL) {
             if (errno != 0)
                 perror("getgrnam()");
             else
                 fprintf(stderr, "\"%s\" : Group doesn't exist\n", token);
             return -1;
         }
+        token = strtok(NULL, ",");
+    }
 
-        args->gc++;
-    } while ( (token = strtok(NULL, ",")) != NULL);
+    if (!i) {
+        fputs("Group is empty\n", stderr);
+        return -1;
+    }
 
-    return 0;
+    return i;
 }
 
 
+/* @return : -1 to error | 0 success */
 int command_verifier(char *command)
 {
-    if (strlen(command) >= MAX_COMMAND_LEN) {
-        fprintf(stderr, "Comand is too long -> %d characters max\n", MAX_COMMAND_LEN);
+    if (command == NULL || !strcmp(command, "")) {
+        fputs("Command is empty\n", stderr);
         return -1;
     }
-    if (!strcmp(command, "")) {
-        fputs("Command is empty\n", stderr);
+    if (strlen(command) >= MAX_COMMAND_LEN) {
+        fprintf(stderr, "Comand is too long -> %d characters max\n", MAX_COMMAND_LEN);
         return -1;
     }
     if (strchr(command, '\'') != NULL && strchr(command, '"') != NULL) {
