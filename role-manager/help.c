6861c3a7023c974b030c79d224b4c574e9733e11
anderseee <77062357+anderseee@users.noreply.github.com>
2022-01-28 16:10:34 +0100

    Update help.c

--- role-manager/help.c
+++ role-manager/help.c
@@ -1,24 +1,26 @@
 #include <stdio.h>
-#include "role_manager.h"
+
+#include "help.h"
 
 void print_help (int command)
 {
     switch(command) {
     case ADDROLE:
-        puts("\nUsage : addrole <role> [-C Capability] [-u user] [-g group] [-c command]");
-        puts("Add a role for be using with sr.\nOptions :");
-        puts(" -C, --capability=cap1,cap2...\tAdd capabilit(y|ies)");
+        puts("\nUsage : addrole <role> <Capability> [-u user] [-g group] [-c command]");
+        puts("Add a role for be using with sr.\n\nMandatory Options :");
+        puts("<role>\t\t: role1\t\t\t\tRole to add.");
+        puts("<Capability>\t: cap_net_raw,cap_sys_net...\tCapabilit(y|ies) to add.\n");
+        puts("Optionnal options :");
         puts(" -u, --user=user1,user2...\tAdd user(s)");
         puts(" -g, --group=group1,group2...\tAdd group(s)");
-        puts(" -c, --command=\"command1\"\tAdd command. Command can be only used with user or group options");
-        puts("\n\t/!\\ 10 users|groups|commands max can be added /!\\\n");
-        puts("Example :");
-        puts("addrole role1 -C cap_net_bind_service -u anderson,ahmed -c \"python server.py -p 80\" -g zayed,university -c \"command\" -c \"command2\"");
+        puts(" -c, --command=\"command1\"\tAdd command.");
+        puts("\nExample :");
+        puts("addrole role1 cap_net_bind_service -u anderson,ahmed -c \"python server.py -p 80\" -g zayed,university -c \"command\" -c \"command2\"\n");
         break;
     case EDITROLE:
-        printf("Usage : editrole <rolename>\n");
+        printf("Usage : editrole <role>\n");
         break;
     case DELETEROLE:
-        printf("Usage : deleterole <rolename>\n");
+        printf("Usage : deleterole <role>\n");
     }
 }
