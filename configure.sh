8d13165f1d0a5f96ac7b19aea775ffa5a1bffba2
LeChatP <lechatp@outlook.fr>
2022-11-03 11:22:32 +0100

    configure for ArchLinux

--- configure.sh
+++ configure.sh
@@ -7,10 +7,16 @@ if [ "$SUDO_USER" = "" ]; then
 fi;
 
 echo "Capabilities & PAM packages installation"
-apt-get install gcc llvm clang || exit
-apt-get install libcap2 libcap2-bin libcap-dev libcap-ng-dev libelf-dev || exit
-apt-get install libpam0g-dev || exit
-apt-get install libxml2 libxml2-dev || exit
+if [ $(which apt-get >/dev/null 2>&1 ; echo $?) -eq 0 ];then 
+	apt-get install gcc llvm clang libcap2 libcap2-bin libcap-dev libcap-ng-dev libelf-dev libpam0g-dev libxml2 libxml2-dev || exit
+elif [ $(which yum >/dev/null 2>&1 ; echo $?) -eq 0 ];then 
+	echo "yum"
+elif [ $(which pacman >/dev/null 2>&1 ; echo $?) -eq 0 ];then 
+	pacman -S gcc llvm clang libcap libcap-ng libelf libxml2 linux-api-headers libpam
+else
+	echo "Unable to find a supported package manager, exiting..."
+	exit 2
+fi
 
 echo "Configuration files installation"
 cp resources/sr_pam.conf /etc/pam.d/sr || exit
