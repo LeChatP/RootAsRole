7ee67f9af24cf598d4b699c29f47a99344f417c9
LeChatP <lechatp@outlook.fr>
2023-07-16 07:48:26 +0200

    remove old python and old tests

--- tests/func/configure.sh
+++
@@ -1,4 +0,0 @@
-sudo apt-get update
-sudo apt-get install python3 -y
-sudo apt-get install build-essential libssl-dev libffi-dev python-dev -y
-sudo apt-get install python3-unittest2
--- tests/resources/apache2.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-source /etc/apache2/envvars
-/usr/sbin/apache2 -k start
