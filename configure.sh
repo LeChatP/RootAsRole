388f0304a4b0021c8db82636e351a17d126f54df
LeChatP <lechatp@outlook.fr>
2024-04-26 16:22:56 +0200

    Configure script for non sudo execution

--- configure.sh
+++ configure.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+
+if [ -z ${SUDO_USER+x} ]; then INSTALL_USER=`id -urn`; else INSTALL_USER=$SUDO_USER; fi
+
 if [ -e "/etc/security/rootasrole.json" ];then
 	read -r -p "Reconfigure policy? [y/N] " response
 	case "$response" in
@@ -10,8 +13,8 @@ if [ -e "/etc/security/rootasrole.json" ];then
 fi
 chmod 0644 /etc/pam.d/sr || exit
 cp resources/rootasrole.json /etc/security || exit
-echo "Define root role for the user ${SUDO_USER}:"
-sed -i "s/ROOTADMINISTRATOR/${SUDO_USER}/g" /etc/security/rootasrole.json
+echo "Define root role for the user $INSTALL_USER:"
+sed -i "s/ROOTADMINISTRATOR/$INSTALL_USER/g" /etc/security/rootasrole.json
 chmod 0640 /etc/security/rootasrole.json || exit
 if [ $DOCKER -eq 0 ]; then
 	chattr +i /etc/security/rootasrole.json || exit
