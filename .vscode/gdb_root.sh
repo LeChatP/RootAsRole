97702de48c58d7a770e8d8c3204ceeae0999ef1e
LeChatP <lechatp@outlook.fr>
2026-07-01 09:43:11 +0200

    chore: remove old artifacts from vscode

--- .vscode/gdb_root.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-SELF_PATH=$(realpath -s "$0")
-
-if [[ "$SUDO_ASKPASS" = "$SELF_PATH" ]]; then
-    zenity --password --title="$1"
-else
-    exec env SUDO_ASKPASS="$SELF_PATH" sudo -A /usr/bin/gdb $@
-fi
